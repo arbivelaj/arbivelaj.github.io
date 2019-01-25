e87d4f8b4ae229a20c7cca2f622ef953ccb1f58c
Arbi Velaj <arbi.velaj@gmail.com>
2019-01-25 18:09:22 +0100

    everything working but the carousel for timeline

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -6,4 +6,14 @@
 
 
 
-
+$('.multi-item-carousel .item').each(function(){
+  console.log("hello");
+  next = $(this).next();
+  if (!next.length) next = $(this).siblings(':first');
+  next.children(':first-child').clone().appendTo($(this));
+});
+$('.multi-item-carousel .item').each(function(){
+  prev = $(this).prev();
+  if (!prev.length) prev = $(this).siblings(':last');
+  prev.children(':nth-last-child(2)').clone().prependTo($(this));
+});

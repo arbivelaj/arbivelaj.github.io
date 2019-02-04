e78eac6823ec6cc4f7eaea5b7b5fc7f15e0d760f
Arbi Velaj <arbi.velaj@gmail.com>
2019-02-04 16:06:33 +0100

    working switcher in sec2

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,18 +2,12 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require navbar_scroll
+//= require flickity.pkgd.min
+//= require course_picker
 //= require_tree .
 
-
-
-$('.multi-item-carousel .item').each(function(){
-  console.log("hello");
-  next = $(this).next();
-  if (!next.length) next = $(this).siblings(':first');
-  next.children(':first-child').clone().appendTo($(this));
-});
-$('.multi-item-carousel .item').each(function(){
-  prev = $(this).prev();
-  if (!prev.length) prev = $(this).siblings(':last');
-  prev.children(':nth-last-child(2)').clone().prependTo($(this));
+$('.carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
 });
